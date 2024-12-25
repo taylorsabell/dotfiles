61fbfc8395db26b47beb95563df11894c65826be
Taylor SaBell <9723132+taylorsabell@users.noreply.github.com>
2024-12-25 15:26:52 -0700

    Exclude a bunch of stuff I don't want copied to my home directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,35 @@ git pull origin main;
 
 function doIt() {
 	rsync --exclude ".git/" \
+		--exclude  ".vim/" \
+		--exclude  "bin/" \
+		--exclude  "init/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude .bash_profile \
+		--exclude .bash_prompt \
+		--exclude .bashrc \
+		--exclude .curlrc \
+		--exclude .editorconfig \
+		--exclude .exports \
+		--exclude .functions \
+		--exclude .gdbinit \
+		--exclude .gitattributes \
+		--exclude .gitignore \
+		--exclude .gvimrc \
+		--exclude .hgignore \
+		--exclude .hushlogin \
+		--exclude .inputrc \
+		--exclude .osx \
+		--exclude .screenrc \
+		--exclude .tmux.conf \
+		--exclude .vimrc \
+		--exclude .wgetrc \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.zprofile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
